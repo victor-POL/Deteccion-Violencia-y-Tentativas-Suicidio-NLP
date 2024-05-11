715f89ad168d8a95435f8094482cd9889955b924
victor-POL <correovictorp@gmail.com>
2024-05-10 22:06:56 -0300

    Fixes extension

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,10 +1,13 @@
-const mensaje = document.querySelector("div");
 const boton = document.getElementById("cambiar-mensaje");
-
-api_url = "https://ed7f-2802-8010-8f23-501-597d-7688-888c-50ea.ngrok-free.app/";
+api_url = "https://73e7-35-230-169-58.ngrok-free.app/";
 
 boton.addEventListener("click", () => {
-  fetch(api_url + "test", {
+  const mensaje = document.querySelector("div");
+  const inputText = encodeURIComponent(
+    document.getElementById("texto-input").value
+  );
+
+  fetch(api_url + "output?text=" + inputText, {
     headers: {
       "ngrok-skip-browser-warning": "true",
     },
@@ -13,13 +16,12 @@ boton.addEventListener("click", () => {
       if (response.ok) {
         return response.json();
       }
-      throw new Error("Error en la solicitud a la API");
+      throw new Error();
     })
     .then((data) => {
-      mensaje.textContent = data.message;
+      mensaje.textContent = data.prediction ? "Suicida" : "No suicida";
     })
     .catch((error) => {
-      console.error("Error:", error);
-      mensaje.textContent = "Error al cargar los datos";
+      mensaje.textContent = "Error al consultar la API";
     });
 });
