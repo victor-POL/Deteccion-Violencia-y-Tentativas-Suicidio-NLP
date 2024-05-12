7ad9b9c1b4dbb06efd88a6a91f061ae6e6d799cb
victor-POL <correovictorp@gmail.com>
2024-05-12 13:30:19 -0300

    Extension con estilos

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,6 +1,6 @@
 const api_url = "http://127.0.0.1:5000/";
-const ruta_text_prediction = "text_prediction"
-const ruta_url_prediction = "url_predition"
+const ruta_text_prediction = "text_prediction";
+const ruta_url_prediction = "url_predition";
 
 const botonObtenerPrediccion = document.getElementById(
   "boton-obtener-prediccion"
@@ -9,37 +9,109 @@ const botonObtenerPaginaActual = document.getElementById(
   "boton-obtener-pagina-actual"
 );
 
+/* -------------------------------------------------------------------------- */
+/*                            Funciones auxiliares                            */
+/* -------------------------------------------------------------------------- */
+
+function obtenerCodigoSpinner() {
+  return `
+    <div class="spinner-border text-light" role="status">
+      <span class="visually-hidden">Cargando...</span>
+    </div>
+  `;
+}
+
+function setearleSpinner(div) {
+  div.className = "";
+  div.innerHTML = obtenerCodigoSpinner();
+}
+
+function setearResultadoPrediccion(div, prediction) {
+  div.textContent = prediction ? "Suicida" : "No suicida";
+  div.className = prediction ? "text-danger" : "text-success";
+}
+
+function setearMensajeErrorAPI(div, error) {
+  div.textContent = error;
+  div.className = "text-primary";
+}
+
+function setearMensajeErrorFetch(div, error) {
+  div.textContent = error;
+  div.className = "text-warning";
+}
+
+function setearMensajeInfo(div, mensaje) {
+  div.textContent = mensaje;
+  div.className = "text-info";
+}
+
+function setearResultado(div, data) {
+  if (typeof data.prediction !== "undefined") {
+    setearResultadoPrediccion(div, data.prediction);
+  } else {
+    setearMensajeErrorAPI(div, data.error);
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+/*                          Obtener predict por input                         */
+/* -------------------------------------------------------------------------- */
+
 botonObtenerPrediccion.addEventListener("click", () => {
-  const divResultadoPrediccion = document.querySelector("#resultado-prediccion");
+  const divResultadoPrediccion = document.querySelector(
+    "#resultado-prediccion"
+  );
+
   const inputText = encodeURIComponent(
     document.getElementById("texto-input").value
   );
 
-  fetch(api_url + ruta_text_prediction + "?text=" + inputText, {
-    headers: {
-      "ngrok-skip-browser-warning": "true",
-    },
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error("Error al enviar el texto a la API");
+  if (inputText === "") {
+    setearMensajeInfo(divResultadoPrediccion, "El campo de texto esta vacio");
+    return;
+  } else {
+    setearleSpinner(divResultadoPrediccion);
+
+    fetch(api_url + ruta_text_prediction + "?text=" + inputText, {
+      headers: {
+        "ngrok-skip-browser-warning": "true",
+      },
     })
-    .then((data) => {
-      divResultadoPrediccion.textContent = data.prediction ? "Suicida" : "No suicida";
-    })
-    .catch((error) => {
-      divResultadoPrediccion.textContent = error.message;
-    });
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Error al enviar el texto a la API");
+      })
+      .then((data) => {
+        setearResultado(divResultadoPrediccion, data);
+      })
+      .catch((error) => {
+        setearMensajeErrorFetch(divResultadoPrediccion, error.message);
+      });
+  }
 });
 
+/* -------------------------------------------------------------------------- */
+/*                           Obtener predict por url                          */
+/* -------------------------------------------------------------------------- */
+
 botonObtenerPaginaActual.addEventListener("click", () => {
-  const divResultadoPaginaActual = document.querySelector("#resultado-pagina-actual");
+  const divResultadoPaginaActual = document.querySelector(
+    "#resultado-pagina-actual"
+  );
 
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const url = tabs[0].url;
-    if (/https:\/\/www\.reddit\.com\/r\/SuicideWatch\/comments\//.test(url)) {
+    if (!/https:\/\/www\.reddit\.com\/r\/SuicideWatch\/comments\//.test(url)) {
+      setearMensajeInfo(
+        divResultadoPaginaActual,
+        "La pagina actual no es de un post de Reddit en r/SuicideWatch"
+      );
+    } else {
+      setearleSpinner(divResultadoPaginaActual);
+
       fetch(api_url + ruta_url_prediction, {
         method: "POST",
         headers: {
@@ -54,17 +126,11 @@ botonObtenerPaginaActual.addEventListener("click", () => {
           throw new Error("Error al enviar la URL a la API");
         })
         .then((data) => {
-          if(data.prediction){
-            divResultadoPaginaActual.textContent = data.prediction ? "Suicida" : "No suicida";
-          } else {
-            divResultadoPaginaActual.textContent = data.error
-          }
+          setearResultado(divResultadoPaginaActual, data);
         })
         .catch((error) => {
-          divResultadoPaginaActual.textContent = error.message
+          setearMensajeErrorFetch(divResultadoPaginaActual, error.message);
         });
-    } else {
-      divResultadoPaginaActual.textContent = "No estás en la página de Reddit de SuicideWatch";
     }
   });
 });
